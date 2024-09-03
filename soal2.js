4893b81c0741b52affacd5d805dc05077b19a8a8
nadiahindrianti <104046022+nadiahindrianti@users.noreply.github.com>
2024-09-03 20:51:38 +0700

    pertanyaan2

---
+++ soal2.js
@@ -0,0 +1,13 @@
+var ditemukan = false;
+
+function (var i=0; i<array.length; i++){
+    if (array[i] ===outputingin) {
+        ditemukan = true;
+        break
+    }
+}
+
+const array = [8 ,3 ,9 ,2 ,5];
+const outputingin = 1;
+
+console.log(ditemukan ? 'true' : 'false');
